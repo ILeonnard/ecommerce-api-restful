4dac06c905f2e1f41c1183d6822002b3c49e61c0
ILeonnard <igorleonnard@gmail.com>
2025-08-27 20:22:48 -0300

    Project Created

---
+++ src/main/java/com/ileonnard/ecommerce_api_restful/EcommerceApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.ileonnard.ecommerce_api_restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApiRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ileonnard/ecommerce_api_restful/EcommerceApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ileonnard.ecommerce_api_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
